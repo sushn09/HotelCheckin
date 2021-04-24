9ab165759525b9ce761ae172d0e573fc57d746f6
sushn09 <sushmita.nandy18@st.niituniversity.in>
2021-04-24 19:16:23 +0530

    Insert Data to Database

--- enroll/views.py
+++ enroll/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .forms import CustomerRegistration
+from .models import User
 
 # Create your views here.
 def add_show(request):
-    return render(request, 'enroll/add_and_show.html')
+    if request.method == 'POST':
+        fm = CustomerRegistration(request.POST)
+        if fm.is_valid():
+            nm = fm.cleaned_data['name']
+            em = fm.cleaned_data['email']
+            pw = fm.cleaned_data['password']
+            reg = User(name=nm, email=em, password=pw)
+            reg.save()
+            fm = CustomerRegistration()
+    else:
+        fm = CustomerRegistration()
+        # cus = User.objects.all()
+    return render(request, 'enroll/add_and_show.html', {'form':fm})

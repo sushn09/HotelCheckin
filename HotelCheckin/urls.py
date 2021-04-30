c20d0b8db2978ca46ff42215f2b69cfe70cf7f0b
sushn09 <sushmita.nandy18@st.niituniversity.in>
2021-04-30 15:59:26 +0530

    Class based View

--- HotelCheckin/urls.py
+++ HotelCheckin/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.create_show.as_view(), name="create_and_show"),
     path('retrieve-record', views.retrieve_record.as_view(), name="retrieve_records"),
+    path('<int:pk>/update', views.update_record.as_view(), name="update_records"),
     path('<int:id>/', views.delete_record.as_view(), name="delete_records"),
-    path('<int:id>/', views.update_record.as_view(), name="update_records"),
-    # path('', views.image_upload, name="image_upload"), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- enroll/views.py
+++ enroll/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from .forms import CustomerRegistration
 from .models import User
 from django.views.generic.base import TemplateView, RedirectView
-from django.views.generic import ListView
+from django.views.generic import ListView, UpdateView
 from django.views import View
 
 # Create your views here.
@@ -36,60 +36,23 @@ class create_show(TemplateView):
             reg.save()
             return HttpResponseRedirect('/')
 
-def create_shows(request):
-    if request.method == 'POST':
-        fm = CustomerRegistration(request.POST, request.FILES)
-        if fm.is_valid():
-            nm = fm.cleaned_data['name']
-            em = fm.cleaned_data['email']
-            mob = fm.cleaned_data['mobileno']
-            gs = fm.cleaned_data['guests']
-            rm = fm.cleaned_data['room']
-            stay = fm.cleaned_data['nightstay']
-            indt = fm.cleaned_data['indate']
-            intm = fm.cleaned_data['intime']
-            outdt = fm.cleaned_data['outdate']
-            outtm = fm.cleaned_data['outtime']
-            amt = fm.cleaned_data['amount']
-            pay = fm.cleaned_data['paymethod']
-            pic = fm.cleaned_data['photo']
-            agree = fm.cleaned_data['agree']
-            reg = User(name=nm, email=em, mobileno=mob, guests=gs, room=rm, nightstay=stay, indate=indt, intime=intm, outdate=outdt, outtime=outtm, amount=amt, paymethod=pay, photo=pic, agree=agree)
-            reg.save()
-            fm = CustomerRegistration()
-            
-    else:
-        fm = CustomerRegistration()
-    cus = User.objects.all()
-    return render(request, 'enroll/add_and_show.html', {'cus':cus,'form':fm})
-
-def retrieve_records(request):    
-    cus = User.objects.all()
-    return render(request, 'enroll/view_records.html', {'cus':cus})
-
 class retrieve_record(ListView):  
     model = User
     def get(self, request):
         fm = CustomerRegistration()
         cus = User.objects.all()
-        return render(request, 'enroll/view_records.html', context={'fm':fm, 'cus':cus})
+        return render(request, 'enroll/view_records.html', context={'form':fm, 'cus':cus})
 
 class delete_record(RedirectView):
     url = '/'
     def get_redirect_url(self, *args, **kwargs):
-        del_id = kwargs['id']
+        del_id = kwargs['id'] 
         User.objects.get(pk=del_id).delete()
         return super().get_redirect_url(*args, **kwargs)
 
-class update_record(View):
-    def get(self, request, id):
-        fr = User.objects.get(pk=id)
-        fm = CustomerRegistration(instance=fr)
-        return render(request, 'enroll/update_customer.html', {'form':fm})
-
-    def post(self, request, id):
-        fr = User.objects.get(pk=id)
-        fm = CustomerRegistration(request.POST, instance=fr)
-        if fm.is_valid():
-            fm.save()
-        return render(request, 'enroll/update_customer.html', {'form':fm})
+class update_record(UpdateView):
+    model = User
+    template_name = 'enroll/update_customer.html'
+    fields = '__all__'
+    form=CustomerRegistration()
+    success_url = "/"
